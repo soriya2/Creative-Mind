c6fd806d5389451338bc8228ad2303b2e4198521
soriya.raksmey <soriya.raksmey@prasac.com.kh>
2022-12-20 11:47:46 +0700

    pwa add

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;400;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+        <meta name="theme-color" content="#fff" />
       </Head>
       <body>
         <Main />
